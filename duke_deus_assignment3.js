ac50cf28f36a0041e24f00382c70aaab48d0bd2d
Deus Duke <deus@deusenterprises.com>
2013-02-21 20:47:20 -0600

    - About half way through the requirements for the JS code

--- duke_deus_assignment3.js
+++ duke_deus_assignment3.js
@@ -1,1 +1,34 @@
-alert("JavaScript works!");
+// Deus Duke
+// SDI 1302
+// Project 3
+// Third project for SDI at Full Sail University
+
+
+var room = {
+	feetSquare: 12,		// square footage
+	furnitureCount: 4	// items of funiture in the room
+};
+
+var rooms = {
+	{"feetSquared": "12", "furnitureCount": "4"},
+	{"feetSquared": "25", "furnitureCount": "8"},
+	{"feetSquared": "10", "furnitureCount": "3"}
+};
+
+// declare roomba object
+var roomba = {};
+
+// object variables
+roomba.timeToClean = true;
+roomba.roomsToClean = rooms;
+roomba.name = "Roku";
+roomba.versionNumber = 770;
+roomba.errorCodes = ["Pass", "Fail"];
+
+roomba.roomCleanTime = function (room: Object) {
+	return (room.feetSquared * furnitureCount) / 3;
+}
+
+roomba.cleanRooms = function () {
+
+};
